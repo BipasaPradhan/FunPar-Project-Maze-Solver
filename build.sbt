26634f91f8f95c699d5debc82044a659ad9a6f11
Bipasa Pradhan <bipspradhan45@gmail.com>
2025-03-20 21:29:34 +0700

    initial commit with reading maze from console

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "MazeSolver"
+
+version := "0.1"
+
+scalaVersion := "3.6.2"  
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.9" % Test
+
+mainClass in Compile := Some("Main")
---
+++ src/main/scala/MazeInput.scala
@@ -0,0 +1,30 @@
+import scala.io.StdIn._
+
+object MazeInput {
+    // Represents a cell of the maze
+    case class Node(row: Int, col: Int)
+
+    // Directions for movement: left, right, down, up
+    val directions = List((-1, 0), (1, 0), (0, -1), (0, 1))
+
+    def readMaze(): Array[Array[Int]] = {
+        println("Start reading the maze")
+        println("Enter the number of rows:")
+        val rows = readInt()
+        println("Rows entered: " + rows)
+
+        println("Enter the number of columns:")
+        val cols = readInt()
+        println("Columns entered: " + cols)
+
+        println(s"Enter the maze (${rows}x${cols}) row by row:")
+        val maze = Array.ofDim[Int](rows, cols)
+
+        for (i <- 0 until rows) {
+            println(s"Reading row $i")
+            val row = readLine().split(" ").map(_.toInt)
+            maze(i) = row
+        }
+        maze
+    }
+}
