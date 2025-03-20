3d625f1b40f66374f80a2b667c1701c6f986db43
Bipasa Pradhan <bipspradhan45@gmail.com>
2025-03-20 21:54:45 +0700

    added in more tests

--- src/test/scala/MazeInputTest.scala
+++ src/test/scala/MazeInputTest.scala
@@ -8,7 +8,7 @@ class MazeInputTest extends AnyFunSuite {
     val input = "2\n2\n1 0\n0 1\n"  //simulate input for 2x2
 
     //simulate user input 
-    val inStream = new java.io.ByteArrayInputStream(input.getBytes)
+    val inStream = new ByteArrayInputStream(input.getBytes)
     Console.withIn(inStream) {
       val maze = readMaze()
       
@@ -19,6 +19,73 @@ class MazeInputTest extends AnyFunSuite {
       )
       
       //comparing each row 
+      for (i <- maze.indices) {
+        assert(maze(i).sameElements(expected(i)), s"Row $i doesn't match")
+      }
+    }
+  }
+
+  test("readMaze should correctly read a 5x5 maze") {
+    val input = "5\n5\n1 0 0 0 0\n1 1 1 0 0\n0 0 1 0 0\n0 0 1 1 1\n0 0 0 0 1\n"  
+    
+    val inStream = new ByteArrayInputStream(input.getBytes)
+    Console.withIn(inStream) {
+      val maze = readMaze()
+      val expected = Array(
+        Array(1, 0, 0, 0, 0),
+        Array(1, 1, 1, 0, 0),
+        Array(0, 0, 1, 0, 0),
+        Array(0, 0, 1, 1, 1),
+        Array(0, 0, 0, 0, 1)
+      )
+      for (i <- maze.indices) {
+        assert(maze(i).sameElements(expected(i)), s"Row $i doesn't match")
+      }
+    }
+  }
+
+  test("readMaze should correctly handle a maze with no valid path (in cases for pathfinding later)") {
+    val input = "3\n3\n1 0 0\n0 0 0\n0 0 1\n"  
+
+    val inStream = new ByteArrayInputStream(input.getBytes)
+    Console.withIn(inStream) {
+      val maze = readMaze()
+      val expected = Array(
+        Array(1, 0, 0),
+        Array(0, 0, 0),
+        Array(0, 0, 1)
+      )
+      for (i <- maze.indices) {
+        assert(maze(i).sameElements(expected(i)), s"Row $i doesn't match")
+      }
+    }
+  }
+
+  test("readMaze should correctly handle a single-row maze") {
+    val input = "1\n5\n1 1 0 1 1\n"  
+    
+    val inStream = new ByteArrayInputStream(input.getBytes)
+    
+    Console.withIn(inStream) {
+        val maze = readMaze()
+        val expected = Array(Array(1, 1, 0, 1, 1))
+        
+        for (i <- maze.indices) {assert(maze(i).sameElements(expected(i)), s"Row $i doesn't match")
+        }
+    }
+  }
+
+  test("readMaze should correctly handle a 3x3 maze") {
+    val input = "3\n3\n1 0 0\n1 0 0\n1 1 1\n"  
+
+    val inStream = new ByteArrayInputStream(input.getBytes)
+    Console.withIn(inStream) {
+      val maze = readMaze()
+      val expected = Array(
+        Array(1, 0, 0),
+        Array(1, 0, 0),
+        Array(1, 1, 1)
+      )
       for (i <- maze.indices) {
         assert(maze(i).sameElements(expected(i)), s"Row $i doesn't match")
       }
