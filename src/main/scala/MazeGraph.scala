ede2afee94a60771a0c98ee4c4257801563b387a
Bipasa Pradhan <bipspradhan45@gmail.com>
2025-03-21 15:56:29 +0700

    turned input matrix into adjacency list

---
+++ src/main/scala/MazeGraph.scala
@@ -0,0 +1,36 @@
+import scala.collection.mutable
+
+//convert given maze into adjacency list for easier lookup
+object MazeGraph {
+    //represents a cell of the maze
+    case class Node(row: Int, col: Int)
+
+    //directions for movement: up, down, left, right
+    val directions = List((-1, 0), (1, 0), (0, -1), (0, 1))
+
+    //returns each node mapped to neighboring nodes it can move to 
+    def mazeToGraph(maze: Array[Array[Int]]): Map[Node, List[(Node, Int)]] = {
+
+        //map of walkable nodes with its walkable neighbors and their weights
+        val graph = mutable.Map[Node, List[(Node, Int)]]().withDefaultValue(List())
+
+        //loop through each row and column, and only process walkable cells (1 = walkable, 0 = wall)
+        for (row <- maze.indices; col <- maze(0).indices if maze(row)(col) == 1) {
+            val node = Node(row, col)
+            //find neighboring nodes
+            val neighbors = directions.flatMap {
+                case (direction_r, direction_c) =>
+                    val newRow = row + direction_r
+                    val newCol = col + direction_c
+
+                    if (newRow >= 0 && newRow < maze.length && newCol >= 0 && newCol < maze(0).length && maze(newRow)(newCol) == 1) {
+                        Some(Node(newRow, newCol) -> 1)
+                    } else {
+                        None
+                    }
+            }
+            graph(node) = neighbors 
+        }
+        graph.toMap
+    }
+}
