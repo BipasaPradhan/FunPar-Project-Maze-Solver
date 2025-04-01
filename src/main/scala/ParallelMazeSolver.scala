cef99b257c7f39d59a1d8f71a35e55e76796b7ff
Bipasa Pradhan <bipspradhan45@gmail.com>
2025-04-02 00:10:58 +0700

    implementing parallel dijkstra with thread safe operations

--- src/main/scala/ParallelMazeSolver.scala
+++ src/main/scala/ParallelMazeSolver.scala
@@ -1,7 +1,8 @@
 import java.util.concurrent.{
   ConcurrentHashMap,
   Executors,
-  PriorityBlockingQueue
+  PriorityBlockingQueue,
+  AtomicBoolean
 }
 import scala.collection.mutable
 import scala.jdk.CollectionConverters._
@@ -15,15 +16,42 @@ object ParallelMazeSolver {
     // stores shortest distance to each viable node
     val distances =
       new ConcurrentHashMap[Node, Int]().asScala.withDefaultValue(Int.MaxValue)
-    distances(start) = 0
+    distances.put(start, 0)
 
     // track the previous node (node before reaching current node)
     val previous = new ConcurrentHashMap[Node, Node]().asScala
 
-    val pq = new PriorityBlockingQueue[(Node, Int)]()(
+    val pq = new PriorityBlockingQueue[(Node, Int)](
+      100,
       Ordering.by[(Node, Int), Int](_._2).reverse
-    ) // min-heap
-    pq.enqueue((start, 0)) // start with the start node
+    )
+    pq.put((start, 0))
+
+    val numThreads = 4
+    // threadpool with fixed number of threads
+    val executor = Executors.newFixedThreadPool(numThreads)
+
+    // flag to stop all threads when the end node is reached
+    val stopFlag = new AtomicBoolean(false)
+
+    val workers = (1 to numThreads).map { _ =>
+      executor.submit(new Runnable {
+        override def run(): Unit = {
+          while (!stopFlag.get() && !pq.isEmpty) {
+            val (currNode, currDis) = pq.poll()
+
+            // skip nodes for which a shorter path is already found
+            if (currDis > distances(currNode)) {
+            } else {
+              // Reached the end -> reconstruct the path from end to beginning & prepend
+              if (currNode == end) {
+                var path = List[Node]()
+                var node = end
+              }
+            }
+        }
+      })
+    }
 
   }
 }
